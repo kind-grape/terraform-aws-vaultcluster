d4e4224690dd03866b139938944b70fe73b729f7
Adam Melong <adam@melong.ca>
2019-07-24 16:15:18 -0400

    more changes

--- 0-initialize/main.tf
+++ 0-initialize/main.tf
@@ -1,6 +1,6 @@
 module "initialize" {
   source = "git@github.com:melonger/aws-initialize.git"
 
-  terraform_bucket_name = "${lower(var.tags["client"])}-s3_bucket"
+  terraform_bucket_name = "${lower(var.tags["client"])}-s3"
   tags                  = "${var.tags}"
 }
