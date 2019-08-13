7c24273e7b4aa2f2d1c50aaf333767efd3bef48f
Adam Melong <adam@melong.ca>
2019-08-13 15:18:25 -0400

    .gitignore fix

--- 0-setup/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "falcon-tfstate"
-    key    = "CLIENT/setup/terraform.tfstate"
-    region = "ca-central-1"
-  }
-}
--- 1-env/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "falcon-tfstate"
-    key    = "GWL/env/terraform.tfstate"
-    region = "ca-central-1"
-  }
-}
