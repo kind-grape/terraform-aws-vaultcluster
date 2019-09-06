0f011ecdf69fd6fe1f8ce9ef7d36d8859123d80a
Adam Melong <adam@melong.ca>
2019-09-06 19:17:34 -0400

    added the CA file to vault consul storage conf, soas not to use local CA

--- modules/aws-hashi_user_data/main.tf
+++ modules/aws-hashi_user_data/main.tf
@@ -19,6 +19,7 @@ data "template_file" "user_data" {
     consul_dns_port         = "${var.ports["consulbk_dns_port"]}"
     vault_api_port          = "${var.ports["vault_api_port"]}"
     vault_cluster_port      = "${var.ports["vault_cluster_port"]}"
+    vault_https_enabled     = "${var.serverinfo["tlslistener"]}"
     vault_telemtry_enabled  = "${var.vault_telemetry}"
     unseal_cloud            = "${var.unseal_cloud}"
     vault_kms_key_id        = "${var.kms_key_id}"
