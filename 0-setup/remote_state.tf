954ac76d3da78722e9097d2953d7b3fd44abd071
Adam Melong <adam@melong.ca>
2019-11-18 12:40:04 -0500

    working tf12 vpc setup

--- 0-setup/remote_state.tf
+++ 0-setup/remote_state.tf
@@ -1,9 +1,9 @@
-data "terraform_remote_state" "setup" {
-  backend = "${var.backend}"
-
-  config {
-    bucket = "${var.bucket}"
-    key    = "${upper(var.tags["client"])}/${upper(var.environment)}/terraform.tfstate"
-    region = "${var.region}"
-  }
-}
+# data "terraform_remote_state" "setup" {
+#   backend = "${var.backend}"
+#
+#   config {
+#     bucket = "${var.bucket}"
+#     key    = "${upper(var.tags["client"])}/${upper(var.environment)}/terraform.tfstate"
+#     region = "${var.region}"
+#   }
+# }
--- modules/aws-vpc/examples/network-acls/main.tf
+++ modules/aws-vpc/examples/network-acls/main.tf
@@ -15,12 +15,23 @@ module "vpc" {
   elasticache_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
 
   public_dedicated_network_acl = true
-  public_inbound_acl_rules     = "${concat(local.network_acls["default_inbound"], local.network_acls["public_inbound"])}"
-  public_outbound_acl_rules    = "${concat(local.network_acls["default_outbound"], local.network_acls["public_outbound"])}"
+  public_inbound_acl_rules = concat(
+    local.network_acls["default_inbound"],
+    local.network_acls["public_inbound"],
+  )
+  public_outbound_acl_rules = concat(
+    local.network_acls["default_outbound"],
+    local.network_acls["public_outbound"],
+  )
+  elasticache_outbound_acl_rules = concat(
+    local.network_acls["default_outbound"],
+    local.network_acls["elasticache_outbound"],
+  )
 
-  private_dedicated_network_acl = true
+  private_dedicated_network_acl     = true
+  elasticache_dedicated_network_acl = true
 
-  assign_generated_ipv6_cidr_block = true
+  enable_ipv6 = true
 
   enable_nat_gateway = false
   single_nat_gateway = true
@@ -51,7 +62,6 @@ locals {
         cidr_block  = "0.0.0.0/0"
       },
     ]
-
     default_outbound = [
       {
         rule_number = 900
@@ -62,7 +72,6 @@ locals {
         cidr_block  = "0.0.0.0/0"
       },
     ]
-
     public_inbound = [
       {
         rule_number = 100
@@ -96,8 +105,15 @@ locals {
         protocol    = "tcp"
         cidr_block  = "0.0.0.0/0"
       },
+      {
+        rule_number     = 140
+        rule_action     = "allow"
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        ipv6_cidr_block = "::/0"
+      },
     ]
-
     public_outbound = [
       {
         rule_number = 100
@@ -131,6 +147,57 @@ locals {
         protocol    = "tcp"
         cidr_block  = "10.0.100.0/22"
       },
+      {
+        rule_number = 140
+        rule_action = "allow"
+        icmp_code   = -1
+        icmp_type   = 8
+        protocol    = "icmp"
+        cidr_block  = "10.0.0.0/22"
+      },
+      {
+        rule_number     = 150
+        rule_action     = "allow"
+        from_port       = 90
+        to_port         = 90
+        protocol        = "tcp"
+        ipv6_cidr_block = "::/0"
+      },
+    ]
+    elasticache_outbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 140
+        rule_action = "allow"
+        icmp_code   = -1
+        icmp_type   = 12
+        protocol    = "icmp"
+        cidr_block  = "10.0.0.0/22"
+      },
+      {
+        rule_number     = 150
+        rule_action     = "allow"
+        from_port       = 90
+        to_port         = 90
+        protocol        = "tcp"
+        ipv6_cidr_block = "::/0"
+      },
     ]
   }
 }
+
