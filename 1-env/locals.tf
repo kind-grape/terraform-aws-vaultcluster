8ca1430fed8c30b229909cc5eaea8d5325aaa4ae
Adam Melong <adam@melong.ca>
2019-12-12 12:00:33 -0500

    fixed sd variables

--- 1-env/locals.tf
+++ 1-env/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  consul_sd      = merge(var.custom_consul_snap, var.consul_sd)
+  consul_sd      = merge(var.custom_consul_sd, var.consul_sd)
   consul_storage = merge(var.custom_consul_storage, var.consul_storage)
   vault          = merge(var.custom_vault, var.vault)
   consul_snap    = merge(var.custom_consul_snap, var.consul_snap)
