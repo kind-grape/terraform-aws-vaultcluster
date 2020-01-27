1833596994b07dcc04006e9abde84a9129a6ba87
Richard Peng <richp@MacBook-Pro.local>
2020-01-27 13:11:58 -0500

    format locals.tf

--- 1-env/locals.tf
+++ 1-env/locals.tf
@@ -3,7 +3,7 @@ locals {
   consul_storage = merge(var.custom_consul_storage, var.consul_storage)
   vault          = merge(var.custom_vault, var.vault)
   consul_snap    = merge(var.custom_consul_snap, var.consul_snap)
-  snapshots    = merge(var.custom_snapshots, var.snapshots)
+  snapshots      = merge(var.custom_snapshots, var.snapshots)
   ports          = merge(var.custom_ports, var.ports)
   mgmt_subnets   = concat(var.mgmt_subnets, [var.address_space])
   certs          = merge(var.example_cert, var.certs)
